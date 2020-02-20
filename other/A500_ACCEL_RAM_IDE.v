f396e6da8f28ddf4cd626fca636dd003ee368cb5
Renee Cousins <nonarkitten@gmail.com>
2020-02-20 13:42:00 -0700

    Add files via upload

---
+++ other/A500_ACCEL_RAM_IDE.v
@@ -0,0 +1,172 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Create Date:    21:28:36 11/05/2017 
+// Design Name:    Amiga 500 in socket 68000 Accelerator, FastRAM and IDE Interface	 
+// Module Name:    A500_ACCEL_RAM_IDE 
+//
+// Designer Name:  Paul Raspa
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module A500_ACCEL_RAM_IDE(
+    // Control Inputs
+    
+    /* Currently disabled to keep it simple -----  
+    input [1:0] MEMORY_SIZE,
+    ----- */
+
+    input RESET,
+    input CLK,
+    input RW,
+    input AS,
+    input UDS,
+    input LDS,
+    
+    // Address Inputs
+    input [7:0] ADDRESS_LOW,
+    input [23:16] ADDRESS_HIGH,
+    
+    // Data Inputs / Outputs
+    inout [15:12] DATA,
+    
+    // RAM Control Outputs
+    output CE_LOW, CE_HIGH,
+    output OE_LOW, OE_HIGH,
+    output WE_LOW, WE_HIGH
+    );
+
+reg readCycle = 1'b0;
+reg writeCycle = 1'b0;
+reg configured = 1'b0;
+reg shutup = 1'b0;
+reg [3:0] autoConfigData = 4'b0000;
+reg [7:0] autoConfigBase = 8'b00000000;
+
+// Hard coded declerations. Thanks Stephen J. Leary for this nice logic for determining the AUTOCONFIG_RANGE and RAM_ACCESS logic.
+wire AUTOCONFIG_RANGE = ~({ADDRESS_HIGH[23:16]} != {8'hE8}) | AS | shutup | configured;
+wire RAM_ACCESS = ~({ADDRESS_HIGH[23:16]} != {autoConfigBase[7:0]}) | AS | ~configured;
+
+// Detect if current cycle is READ or WRITE.
+always @(posedge CLK, posedge AS) begin
+    if (AS == 1'b1) begin
+        // Rising edge of /AS indicates end of current cycle. Use this to clear flags
+        readCycle  <= 1'b0;
+        writeCycle <= 1'b0;
+    end else begin
+        if (AS == 1'b0) begin
+            // Falling edge of /AS indicates end of S2 of current cycle. Use this to determine READ or WRITE
+            if (RW == 0)
+                readCycle <= 1'b1;
+            else
+                writeCycle <= 1'b1;
+        end
+    end
+end
+
+// Enable (or disable) AUTOCONFIG cycle.
+always @(posedge writeCycle, negedge RESET) begin
+    if (RESET == 1'b0) begin
+        configured <= 1'b0;
+        shutup <= 1'b0;
+    end else begin
+        if (AUTOCONFIG_RANGE == 1'b1) begin
+            // AutoConfig Write sequence. Here is where we receive from the OS the base address for the RAM.
+            case (ADDRESS_LOW)
+                8'b01001000: // $48
+                    autoConfigBase[7:4] <= DATA[15:12];
+                8'b01001010: // $4A
+                    autoConfigBase[3:0] <= DATA[15:12];
+                8'b01001100: begin // $4C                
+                    shutup <= 1'b1;
+                    configured <= 1'b1;
+                end
+                8'b01001110: begin // $4E                                
+                    shutup <= 1'b1;
+                    configured <= 1'b1;
+                end
+                default: begin
+                    shutup <= 1'b1;
+                    configured <= 1'b1;
+                end
+            endcase
+        end
+    end
+end
+
+// Evaluate autoConfigData data based on user-selectable memory configuration
+always @(posedge readCycle) begin
+    if (AUTOCONFIG_RANGE == 1'b1) begin
+        // AutoConfig Read sequence. Here is where we publish the RAM Size and Hardware attributes.
+        case (ADDRESS_LOW)
+            8'b00000000: // $00
+                autoConfigData <= 4'b1110;
+            8'b00000001: // $02
+                // 1 MB Configuration
+                autoConfigData <= 4'b0101;
+
+                /* Currently disabled to keep it simple -----                
+                
+                if (MEMORY_SIZE == 2'b00)
+                    // 1 MB Configuration
+                    autoConfigData <= 4'b0101;
+                else if (MEMORY_SIZE == 2'b01)
+                    // 2 MB Configuration
+                    autoConfigData <= 4'b0110;
+                else if (MEMORY_SIZE == 2'b10)
+                    // 4 MB Configuration
+                    autoConfigData <= 4'b0111;
+                else if (MEMORY_SIZE == 2'b11)
+                    // 8 MB Configuration
+                    autoConfigData <= 4'b0000;
+                else
+                    autoConfigData <= 4'b0101;
+                    
+                ----- */
+            
+            8'b00000010: // $04
+                autoConfigData <= 4'h0;
+            8'b00000011: // $06
+                autoConfigData <= 4'h0;
+            
+            8'b00000100: // $08
+                autoConfigData <= 4'h0;
+            8'b00000101: // $0A
+                autoConfigData <= 4'h0;
+
+            8'b00001000: // $10
+                autoConfigData <= 4'h0;
+            8'b00001001: // $12
+                autoConfigData <= 4'h0;
+            
+            8'b00001010: // $14
+                autoConfigData <= 4'h0;
+            8'b00001011: // $16
+                autoConfigData <= 4'h0;
+
+            8'b00100000: // $40
+                autoConfigData <= 4'b0000;
+            8'b00100001: // $42
+                autoConfigData <= 4'b0000;    
+        
+            default:
+                autoConfigData <= 4'b1111;
+        endcase
+    end
+end
+
+// Output specific AUTOCONFIG data.
+assign DATA = (readCycle == 1'b1 && AUTOCONFIG_RANGE == 1'b1 && configured == 1'b0) ? 4'bZZZZ : autoConfigData;
+
+// RAM Control arbitration.
+assign CE_LOW = ~(RAM_ACCESS & (readCycle == 1'b1 | writeCycle == 1'b1) & ~LDS);
+assign CE_HIGH = ~(RAM_ACCESS & (readCycle == 1'b1 | writeCycle == 1'b1) & ~UDS);
+assign OE_LOW = ~(RAM_ACCESS & (readCycle == 1'b1) & ~LDS);
+assign OE_HIGH = ~(RAM_ACCESS & (readCycle == 1'b1) & ~UDS);
+assign WE_LOW = ~(RAM_ACCESS & (writeCycle == 1'b1) & ~LDS);
+assign WE_HIGH = ~(RAM_ACCESS & (writeCycle == 1'b1) & ~UDS);
+
+endmodule
---
+++ other/Gary.v
@@ -0,0 +1,194 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:24:47 10/29/2015 
+// Design Name: 
+// Module Name:    Gary 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Gary(
+    output nVPA,
+    output nCDR,
+    output nCDW,
+    input nKRES,
+    input nMTR,
+    input nDKWD,
+    input nDKWE,
+    input nUDS,
+    input nLDS,
+    input RW,
+    input nAS,
+    input nBGACK,
+    input nDBR,
+    input nSEL0,
+    output nRGAE,
+    output nBLS,
+    output nRAME,
+    output nROME,
+    output nRTCR,
+    output nRTCW,
+    output reg nLATCH,
+    input nCDAC,
+    input C3,
+    input C1,
+    input nOVR,
+	 input OVL,
+    input XRDY,
+    input nEXP,
+    input [23:17] A,
+    inout nRESET,
+    output nHALT,
+    output nDTACK,
+    output DKWEB,
+    output DKWDB,
+    output MTR0D,
+    output MTRXD
+    );
+	 
+	//internal registers
+	reg AS_14D0,nDBR_D0;
+	reg nDTACK_S, nCDR_S, nCDW_S,nBLS_S, MTR0_S;	
+	reg [7:0]counter;
+	//generate processor clock
+	wire C7M = C3 ~^ C1; // c1 not xor c2 = 7mhz
+	wire C14M = C7M ^ ~nCDAC; //14MHZ
+	wire DS = ~nUDS | ~nLDS;
+
+	wire chipram 	= (~OVL & A[23:21]==3'b000 
+									//| A[23:19]>5'b00001
+								);
+	wire rom			= (	( OVL & A[23:21]==3'b000 ) //rom overlay during start
+									| A[23:19]==5'b11111 	//F80000-FFFFFF
+									| A[23:19]==5'b11100 );	//E00000-E7FFFF
+	wire clock		= A[23:17]==7'b1101110;				//clock: D80000-DB0000
+	wire cia			= A[23:21]==3'b101;					//cia: A00000-BFFFFF
+	wire chipset	= (nEXP &								//expansion selected
+								(A[23:20]==4'b1100			//C00000-CFFFFF
+								|A[23:19]==5'b11010))|		//D00000-D7FFFF
+								A[23:18]==6'b110111;			//chipset
+	wire ranger		= ~nEXP &								//expansion selected
+								(A[23:20]==4'b1100			//C00000-CFFFFF
+								|A[23:19]==5'b11010)			//D00000-D7FFFF
+								;
+	//all others a bit later with AS_14D0
+	wire other		=~chipram & ~rom & ~clock & ~ chipset & ~ranger & ~cia & ~AS_14D0;
+	//reset generation
+	assign nHALT = ~nKRES ? 0 : 1'bz;	
+	assign nRESET = ~nKRES ? 0 : 1'bz;
+		
+	//assign simple signals
+	assign DKWDB = ~nDKWD;
+	assign DKWEB = nDKWE  & nRESET;
+	assign MTRXD = ~nMTR  & nRESET;
+	assign MTR0D = MTR0_S ;
+	//select floppy motor
+	always @(negedge nSEL0 ,negedge  nRESET)
+	begin
+		if( nRESET==0)
+		begin
+			MTR0_S <= 0;
+		end
+		else
+		begin
+			MTR0_S <= ~nMTR;
+		end
+	end
+	
+	//decode address and generate the internal signals
+	always @(posedge C14M)
+	begin
+		//this replaces the nasty latch!
+		nLATCH	<=	C3;
+		AS_14D0	<= nAS;
+		nDBR_D0	<= nDBR;
+		if(nAS)
+		begin
+			nDTACK_S	<=1;
+			nCDR_S	<=1;
+			nCDW_S	<=1;
+			nBLS_S	<=1;
+			counter	<=8'h00;
+		end
+		else
+		begin
+		
+			//count 7Mhz-flanks: odd falling even rising
+			counter <=counter+1; // the cycle starts at S3: this time the first cycle is seen!
+			
+			if( 
+				((~nDBR | ~nDBR_D0) &( //blitting
+					chipram | chipset | ranger //Agnus	
+					)					
+				)					
+				| cia //cia access
+				& nDTACK_S //not asserted
+				
+				)
+			begin
+				nDTACK_S <= 1;
+			end
+			else 
+			begin
+				nDTACK_S <= ~XRDY; //ready to rambo					
+			end
+			
+						
+			//slow down blitter
+			nBLS_S 	<= ~((	chipram | ranger | chipset) & (counter[1:0]>=2'b00 & counter[1:0]<=2'b01));
+
+			//read from RAM / register	
+			if(	counter>=8'h01 //minimum wait					
+					& RW //read
+					& nDBR_D0 & nDBR //no blitting
+					& (chipset | chipram | ranger) //agnus-select
+					& nCDR_S //not asseted
+					)
+			begin
+				nCDR_S	<= 0;
+			end
+			
+			//write to RAM / register
+			if(	~RW //write
+					& nDBR_D0 & nDBR //no blitting
+					& (chipset | chipram | ranger) //agnus-select
+					& nCDW_S //not asseted
+					)
+			begin
+				nCDW_S	<= 0;
+			end
+			
+
+		end
+	end
+	
+	always @(posedge C14M)
+	begin
+	end
+		
+	//output signal generation
+	assign nVPA = nOVR & ~nAS ? ~cia : 1'bz;
+	assign nDTACK = (nOVR & ~nAS ) ? nDTACK_S : 1'bz;
+	assign nROME = nOVR & ~nAS ? ~(rom & RW) : 1; //only on read!
+	assign nRTCR = nOVR & ~nAS ? ~(clock &  RW &  DS) : 1;
+	assign nRTCW = nOVR & ~nAS ? ~(clock & ~RW &  DS) : 1;
+	assign nRAME = nOVR & ~nAS ? ~(chipram | ranger ) : 1;
+	assign nCDR  = nOVR & ~nAS ? nCDR_S : 1;
+	assign nCDW  = nOVR & ~nAS ? nCDW_S : 1;
+	assign nRGAE = nOVR & ~nAS ? ~chipset : 1;
+	assign nBLS	 = nOVR & ~nAS ? nBLS_S : 1;
+	
+
+
+endmodule
